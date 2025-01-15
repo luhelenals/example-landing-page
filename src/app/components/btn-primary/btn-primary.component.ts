9d17caac2f482638c8539c72a570581cce433ee6
Luiza <luizahelena.lsantos@gmail.com>
2025-01-15 14:53:43 -0300

    buttons

--- src/app/components/btn-primary/btn-primary.component.ts
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+type BtnVariants = "primary" | "secondary";
 
 @Component({
-  selector: 'app-btn-primary',
+  selector: 'btn-primary',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './btn-primary.component.html',
   styleUrl: './btn-primary.component.scss'
 })
 export class BtnPrimaryComponent {
+  @Input("btn-text") btnText: string = "";
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() variant: BtnVariants = "primary";
 
+  @Output("submit") onSubmit = new EventEmitter();
+
+  submit() {
+    this.onSubmit.emit();
+  }
 }
